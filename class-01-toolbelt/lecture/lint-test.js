5c626481863ca57ee4bd342c2de83d12ead51644
Sam Hamm <samhamm@gmail.com>
2016-04-11 08:10:33 -0700

    initial commit

---
+++ class-01-toolbelt/lecture/lint-test.js
@@ -0,0 +1,15 @@
+/*This file is to test the ESLint installations of students in Code 201 and guide them through the basics of making edits in response to linter messages.
+
+The following errors are caught by our linter rules:
+
+missing semicolons
+more than one empty line
+extra whitespace
+indenting
+only use single quotes*/
+
+// WHAT ABOUT UNDECLARED VARIABLES BESIDES 'USE STRICT'
+
+bird = 'Big Bird';
+
+console.log(bird);
