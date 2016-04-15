a427fd471f14a8593e643455c9b34bab27f4c503
Sam Hamm <samhamm@gmail.com>
2016-04-15 12:19:17 -0700

    end of class 4/15

--- class-05-images-color-text/demo-lab/js/app.js
+++ class-05-images-color-text/demo-lab/js/app.js
@@ -1,3 +1,5 @@
 function yell(aString) {
-  console.log(aString.toUpperCase()+ '!!!!');
+  console.log(aString.toUpperCase() + '!!!!');
+  var yellEl = document.getElementById('yellEl');
+  yellEl.textContent = aString.toUpperCase() + '!!!!';
 }
